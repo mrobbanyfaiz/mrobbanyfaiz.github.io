3de6bc41ca6a2ad70f4d949845b85e6356982f32
mrobbanyfaiz <49423269+mrobbanyfaiz@users.noreply.github.com>
2019-04-09 11:21:02 +0700

    Add files via upload

---
+++ js/function.js
@@ -0,0 +1,7 @@
+<script type="text/javascript" src="path-to-javascript-file.js"></script>
+/* Demo purposes only */
+$(".hover").mouseleave(
+  function () {
+    $(this).removeClass("hover");
+  }
+);
